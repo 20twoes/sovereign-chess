e11c145d5c40eb8b9935588942e45f03df7d0b85
Al Nguyen <al.nguyen@gmail.com>
2023-12-14 09:56:19 -0500

    Add placeholder pieces

--- README.md
+++ README.md
@@ -17,6 +17,15 @@ For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
+## Development
+
+```
+# Start app for local development
+flutter run
+
+# Then choose Chrome (2) as the target
+```
+
 ## Deploy
 
 We use Firebase hosting to serve the app. For first time setup instructions see [here](https://docs.flutter.dev/deployment/web#deploying-to-firebase-hosting).
--- lib/board/config.dart
+++ lib/board/config.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
-final _black = Colors.black87;
+const _black = Colors.black87;
 
 final colors = (
   black: _black,
   lightSquare: Colors.amber[50],
   darkSquare: Colors.amber[100],
 
-  whiteSquare: Colors.white70,
+  whiteSquare: Colors.white60,
   blackSquare: _black,
   ashSquare: Colors.blueGrey[100],
   slateSquare: Colors.grey,
 
-  pinkSquare: Colors.pink[200],
-  redSquare: Colors.red[700],
-  orangeSquare: Colors.orange[400],
-  yellowSquare: Colors.yellow[400],
+  pinkSquare: Colors.pink[100],
+  redSquare: Colors.red[300],
+  orangeSquare: Colors.orange[300],
+  yellowSquare: Colors.yellow[300],
   greenSquare: Colors.green[300],
   cyanSquare: Colors.cyan[200],
   navySquare: Colors.indigo[300],
-  violetSquare: Colors.deepPurple[400],
+  violetSquare: Colors.deepPurple[300],
+
+  whitePiece: Colors.white60,
+  blackPiece: Colors.black54,
+  ashPiece: Colors.blueGrey[300],
+  slatePiece: Colors.grey[700],
+
+  pinkPiece: Colors.pink,
+  redPiece: Colors.red,
+  orangePiece: Colors.orange,
+  yellowPiece: Colors.yellow,
+  greenPiece: Colors.green,
+  cyanPiece: Colors.cyan,
+  navyPiece: Colors.indigo,
+  violetPiece: Colors.deepPurple,
 );
