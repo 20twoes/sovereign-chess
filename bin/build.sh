e0255140cc0877071864203ad53977dbe6033b0e
Al Nguyen <al.nguyen@gmail.com>
2024-05-01 15:55:55 -0400

    Define API constant

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,1 @@
-flutter build web --release --no-tree-shake-icons --dart-define=WS_URI=wss://yesplz.xyz/ws
+flutter build web --release --no-tree-shake-icons --dart-define=WS_URI=wss://yesplz.xyz/ws --dart-define=API_HOST=https://yesplz.xyz/api
