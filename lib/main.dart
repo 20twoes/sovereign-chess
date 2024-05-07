0e1e7060f2093c821c3a9dbb2f223f76b1ee6ba2
Al Nguyen <al.nguyen@gmail.com>
2024-05-07 13:55:48 -0400

    WIP: rebuilding navigation with go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,249 +1,198 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:web_socket_channel/status.dart' as status;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'src/game/game.dart' as game;
+import 'src/screens/home.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(key: Key('home screen')),
+      routes: [
+        GoRoute(
+          path: 'play',
+          builder: (context, state) {
+            return GameDetailsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+  debugLogDiagnostics: true,
+);
 
 void main() {
-  runApp(SochessApp());
+  runApp(const SochessApp());
 }
 
-class SochessApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _SochessAppState();
-}
-
-class _SochessAppState extends State<SochessApp> {
-  GameRouterDelegate _routerDelegate = GameRouterDelegate();
-  GameRouteInformationParser _routerInformationParser = GameRouteInformationParser();
+class SochessApp extends StatelessWidget {
+  const SochessApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Sovereign Chess',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey.shade900),
-        useMaterial3: true,
-      ),
-      routerDelegate: _routerDelegate,
-      routeInformationParser: _routerInformationParser,
+      routerConfig: _router,
     );
   }
 }
 
-class GameRouteInformationParser extends RouteInformationParser<GameRoutePath> {
-  @override
-  Future<GameRoutePath> parseRouteInformation(RouteInformation routeInformation) async {
-    final uri = Uri.parse(routeInformation.location);
+//class SochessApp extends StatefulWidget {
+//  @override
+//  State<StatefulWidget> createState() => _SochessAppState();
+//}
+//
+//class _SochessAppState extends State<SochessApp> {
+//  //GameRouterDelegate _routerDelegate = GameRouterDelegate();
+//  //GameRouteInformationParser _routerInformationParser = GameRouteInformationParser();
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp.router(
+//      routerConfig: _router,
+//    );
+//    //return MaterialApp.router(
+//    //  title: 'Sovereign Chess',
+//    //  theme: ThemeData(
+//    //    colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey.shade900),
+//    //    useMaterial3: true,
+//    //  ),
+//    //  routerDelegate: _routerDelegate,
+//    //  routeInformationParser: _routerInformationParser,
+//    //);
+//  }
+//}
 
-    // Handle '/'
-    if (uri.pathSegments.length == 0) {
-      return GameRoutePath.home();
-    }
-
-    // Handle '/game/:id'
-    if (uri.pathSegments.length == 2) {
-      if (uri.pathSegments[0] != 'game') {
-        return GameRoutePath.unknown();
-      }
-      var id = uri.pathSegments[1];
-      if (id == null) {
-        return GameRoutePath.unknown();
-      }
-      return GameRoutePath.details(id);
-    }
-
-    // Handle unknown routes
-    return GameRoutePath.unknown();
-  }
-
-  @override
-  RouteInformation? restoreRouteInformation(GameRoutePath path) {
-    if (path.isUnknown) {
-      return RouteInformation(location: '/404');
-    }
-    if (path.isHomePage) {
-      return RouteInformation(location: '/');
-    }
-    if (path.isDetailsPage) {
-      return RouteInformation(location: '/game/${path.id}');
-    }
-    return null;
-  }
-}
-
-class GameRouterDelegate extends RouterDelegate<GameRoutePath>
-  with ChangeNotifier, PopNavigatorRouterDelegateMixin<GameRoutePath> {
-    final GlobalKey<NavigatorState> navigatorKey;
-
-  game.GameForList? _selectedGame;
-  bool show404 = false;
-
-  GameRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
-
-  GameRoutePath get currentConfiguration {
-    if (show404) {
-      return GameRoutePath.unknown();
-    }
-    return _selectedGame == null
-      ? GameRoutePath.home()
-      : GameRoutePath.details(_selectedGame!.id);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Navigator(
-      key: navigatorKey,
-      pages: [
-        MaterialPage(
-          key: ValueKey('GamesListPage'),
-          child: GamesListScreen(
-            onTapped: _handleGameTapped,
-          ),
-        ),
-        if (show404)
-          MaterialPage(key: ValueKey('UnknownPage'), child: UnknownScreen())
-        else if (_selectedGame != null)
-          MaterialPage(key: ValueKey('GameDetailsPage' + _selectedGame!.id), child: GameDetailsScreen(currentGame: _selectedGame))
-      ],
-      onPopPage: (route, result) {
-        if (!route.didPop(result)) {
-          return false;
-        }
-
-        _selectedGame = null;
-        show404 = false;
-        notifyListeners();
-
-        return true;
-      },
-    );
-  }
-
-  @override
-  Future<void> setNewRoutePath(GameRoutePath path) async {
-    if (path.isUnknown) {
-      _selectedGame = null;
-      show404 = true;
-      return;
-    }
-
-    if (path.isDetailsPage) {
-      // TODO: If id is not valid return 404
-      //_selectedGame = games[path.id]; // TODO
-      //_selectedGame = games[0]; // TODO
-    } else {
-      _selectedGame = null;
-    }
-
-    show404 = false;
-  }
-
-  void _handleGameTapped(game.GameForList currentGame) {
-    _selectedGame = currentGame;
-    notifyListeners();
-  }
-}
-
-class GameRoutePath {
-  final String? id;
-  final bool isUnknown;
-
-  GameRoutePath.home() : id = null, isUnknown = false;
-
-  GameRoutePath.details(this.id) : isUnknown = false;
-
-  GameRoutePath.unknown() : id = null, isUnknown = true;
-
-  bool get isHomePage => id == null;
-
-  bool get isDetailsPage => id != null;
-}
-
-class GamesListScreen extends StatefulWidget {
-  final ValueChanged<game.GameForList> onTapped;
-
-  const GamesListScreen({super.key, required this.onTapped});
-
-  @override
-  State<GamesListScreen> createState() => _GamesListScreenState();
-}
-
-class _GamesListScreenState extends State<GamesListScreen> {
-  late Future<List<game.GameForList>> _futureGames;
-
-  @override
-  void initState() {
-    super.initState();
-    _futureGames = game.fetchGames();
-  }
-
-  void _createGame() async {
-    final newGame = await game.createGame();
-    final currentGames = await _futureGames;
-    // Don't need to use setState since we navigate to another page
-    _futureGames = Future.value([...currentGames, newGame]);
-    widget.onTapped(newGame);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Sovereign Chess'),
-      ),
-      body: FutureBuilder<List<game.GameForList>>(
-        future: _futureGames,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-              children: [
-                for (var g in snapshot.data!)
-                  GestureDetector(
-                    child: GameListItem(g),
-                    onTap: () => widget.onTapped(g),
-                  )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
-
-          return const CircularProgressIndicator();
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _createGame,
-        tooltip: 'Start a new game',
-        child: const Text('Play'),
-      ),
-    );
-  }
-}
-
-class GameListItem extends StatelessWidget {
-  final game.GameForList currentGame;
-
-  GameListItem(this.currentGame);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: game.StaticBoard(fenStr: currentGame.fen),
-          ),
-          Expanded(
-            child: Text(currentGame.id),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//class GameRouteInformationParser extends RouteInformationParser<GameRoutePath> {
+//  @override
+//  Future<GameRoutePath> parseRouteInformation(RouteInformation routeInformation) async {
+//    final uri = Uri.parse(routeInformation.location);
+//
+//    // Handle '/'
+//    if (uri.pathSegments.length == 0) {
+//      return GameRoutePath.home();
+//    }
+//
+//    // Handle '/game/:id'
+//    if (uri.pathSegments.length == 2) {
+//      if (uri.pathSegments[0] != 'game') {
+//        return GameRoutePath.unknown();
+//      }
+//      var id = uri.pathSegments[1];
+//      if (id == null) {
+//        return GameRoutePath.unknown();
+//      }
+//      return GameRoutePath.details(id);
+//    }
+//
+//    // Handle unknown routes
+//    return GameRoutePath.unknown();
+//  }
+//
+//  @override
+//  RouteInformation? restoreRouteInformation(GameRoutePath path) {
+//    if (path.isUnknown) {
+//      return RouteInformation(location: '/404');
+//    }
+//    if (path.isHomePage) {
+//      return RouteInformation(location: '/');
+//    }
+//    if (path.isDetailsPage) {
+//      return RouteInformation(location: '/game/${path.id}');
+//    }
+//    return null;
+//  }
+//}
+//
+//class GameRouterDelegate extends RouterDelegate<GameRoutePath>
+//  with ChangeNotifier, PopNavigatorRouterDelegateMixin<GameRoutePath> {
+//    final GlobalKey<NavigatorState> navigatorKey;
+//
+//  game.GameForList? _selectedGame;
+//  bool show404 = false;
+//
+//  GameRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
+//
+//  GameRoutePath get currentConfiguration {
+//    if (show404) {
+//      return GameRoutePath.unknown();
+//    }
+//    return _selectedGame == null
+//      ? GameRoutePath.home()
+//      : GameRoutePath.details(_selectedGame!.id);
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Navigator(
+//      key: navigatorKey,
+//      pages: [
+//        MaterialPage(
+//          key: ValueKey('GamesListPage'),
+//          child: GamesListScreen(
+//            onTapped: _handleGameTapped,
+//          ),
+//        ),
+//        if (show404)
+//          MaterialPage(key: ValueKey('UnknownPage'), child: UnknownScreen())
+//        else if (_selectedGame != null)
+//          MaterialPage(key: ValueKey('GameDetailsPage' + _selectedGame!.id), child: GameDetailsScreen(currentGame: _selectedGame))
+//      ],
+//      onPopPage: (route, result) {
+//        if (!route.didPop(result)) {
+//          return false;
+//        }
+//
+//        _selectedGame = null;
+//        show404 = false;
+//        notifyListeners();
+//
+//        return true;
+//      },
+//    );
+//  }
+//
+//  @override
+//  Future<void> setNewRoutePath(GameRoutePath path) async {
+//    if (path.isUnknown) {
+//      _selectedGame = null;
+//      show404 = true;
+//      return;
+//    }
+//
+//    if (path.isDetailsPage) {
+//      // TODO: If id is not valid return 404
+//      //_selectedGame = games[path.id]; // TODO
+//      //_selectedGame = games[0]; // TODO
+//    } else {
+//      _selectedGame = null;
+//    }
+//
+//    show404 = false;
+//  }
+//
+//  void _handleGameTapped(game.GameForList currentGame) {
+//    _selectedGame = currentGame;
+//    notifyListeners();
+//  }
+//}
+//
+//class GameRoutePath {
+//  final String? id;
+//  final bool isUnknown;
+//
+//  GameRoutePath.home() : id = null, isUnknown = false;
+//
+//  GameRoutePath.details(this.id) : isUnknown = false;
+//
+//  GameRoutePath.unknown() : id = null, isUnknown = true;
+//
+//  bool get isHomePage => id == null;
+//
+//  bool get isDetailsPage => id != null;
+//}
 
 class GameDetailsScreen extends StatefulWidget {
   final game.GameForList? currentGame;
---
+++ lib/src/screens/home.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+import '../game/game.dart' show GameForList, StaticBoard, fetchGames;
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sovereign Chess')),
+      body: GamesData(),
+    );
+  }
+}
+
+class GamesData extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _GamesDataState();
+}
+
+class _GamesDataState extends State<GamesData> {
+  late Future<List<GameForList>> _futureGames;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureGames = fetchGames();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<GameForList>>(
+      future: _futureGames,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GameList(games: snapshot.data!);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      }
+    );
+  }
+}
+
+class GameList extends StatelessWidget {
+  final List<GameForList> games;
+
+  GameList({super.key, required this.games});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (var g in games)
+          GestureDetector(
+            child: GameListItem(g),
+            //onTap: () => widget.onTapped(g),
+          )
+      ],
+    );
+  }
+}
+
+//class GamesListScreen extends StatefulWidget {
+//  final ValueChanged<game.GameForList> onTapped;
+//
+//  const GamesListScreen({super.key, required this.onTapped});
+//
+//  @override
+//  State<GamesListScreen> createState() => _GamesListScreenState();
+//}
+//
+//class _GamesListScreenState extends State<GamesListScreen> {
+//  late Future<List<game.GameForList>> _futureGames;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    _futureGames = game.fetchGames();
+//  }
+//
+//  void _createGame() async {
+//    final newGame = await game.createGame();
+//    final currentGames = await _futureGames;
+//    // Don't need to use setState since we navigate to another page
+//    _futureGames = Future.value([...currentGames, newGame]);
+//    widget.onTapped(newGame);
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//        title: Text('Sovereign Chess'),
+//      ),
+//      body: FutureBuilder<List<game.GameForList>>(
+//        future: _futureGames,
+//        builder: (context, snapshot) {
+//          if (snapshot.hasData) {
+//            return ListView(
+//              children: [
+//                for (var g in snapshot.data!)
+//                  GestureDetector(
+//                    child: GameListItem(g),
+//                    onTap: () => widget.onTapped(g),
+//                  )
+//              ],
+//            );
+//          } else if (snapshot.hasError) {
+//            return Text('${snapshot.error}');
+//          }
+//
+//          return const CircularProgressIndicator();
+//        },
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _createGame,
+//        tooltip: 'Start a new game',
+//        child: const Text('Play'),
+//      ),
+//    );
+//  }
+//}
+
+class GameListItem extends StatelessWidget {
+  final GameForList currentGame;
+
+  GameListItem(this.currentGame);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: StaticBoard(fenStr: currentGame.fen),
+          ),
+          Expanded(
+            child: Text(currentGame.id),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
